6af79cab8ae0be9ae2e83649d2747f15acc57540
oliveirabilu <carloscesaroliveira13@hotmail.com>
2025-11-10 20:02:15 -0300

    feature: Criado pacote Controller e Classe Controller Colocado anotations RestController e REquestMapping com endpoint /enfermeiros e Metodo Post para teste da aplicação

--- enfermeiro/src/main/java/com/exampleenfermeiro/controller/EnfermeiroController.java
+++ enfermeiro/src/main/java/com/exampleenfermeiro/controller/EnfermeiroController.java
@@ -1,9 +1,15 @@
 package com.exampleenfermeiro.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/enfermeiros")
 public class EnfermeiroController {
+    @PostMapping
+    public  void cadastrar (@RequestBody String Json){
+        System.out.println(Json);
+    }
 }
