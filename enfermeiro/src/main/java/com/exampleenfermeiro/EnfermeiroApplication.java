3a90659eea318db77fcb7a8ffe997774cb20003f
oliveirabilu <carloscesaroliveira13@hotmail.com>
2025-11-10 18:02:24 -0300

    first commit

---
+++ enfermeiro/src/main/java/com/exampleenfermeiro/EnfermeiroApplication.java
@@ -0,0 +1,13 @@
+package com.exampleenfermeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnfermeiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnfermeiroApplication.class, args);
+	}
+
+}
---
+++ enfermeiro/src/test/java/com/exampleenfermeiro/EnfermeiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleenfermeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnfermeiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
