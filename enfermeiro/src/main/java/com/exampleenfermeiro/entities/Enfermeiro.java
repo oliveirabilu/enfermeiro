21ded61c853c1dfe3fae71021ae67caa2e536ab6
oliveirabilu <carloscesaroliveira13@hotmail.com>
2025-11-11 15:34:53 -0300

    feature: Colocado a  dependencia Jpa e H2 e colocado aclasse entidade com anotation entity, mostrado a chave primaria com anotation Id

--- enfermeiro/src/main/java/com/exampleenfermeiro/entities/Enfermeiro.java
+++ enfermeiro/src/main/java/com/exampleenfermeiro/entities/Enfermeiro.java
@@ -2,15 +2,21 @@ package com.exampleenfermeiro.entities;
 
 import com.exampleenfermeiro.enumm.Laboratorio;
 import com.exampleenfermeiro.enumm.Via;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
-
+@Entity
+@Table(name = "TB_ENFERMEIRO")
 public class Enfermeiro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Enumerated(EnumType.STRING)
     private Via via;
     private String lote;
     private Integer quantidade;
     private LocalDate validade;
+    @Enumerated(EnumType.STRING)
     private Laboratorio laboratorio;
 }
