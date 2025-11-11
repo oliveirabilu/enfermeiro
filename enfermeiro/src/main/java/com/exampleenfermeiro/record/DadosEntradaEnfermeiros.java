1b585526ea9a202a4aeb62f6b2f9922ceee10941
oliveirabilu <carloscesaroliveira13@hotmail.com>
2025-11-11 15:32:00 -0300

    feature: Colocado a anotation enumerated no s enum

--- enfermeiro/src/main/java/com/exampleenfermeiro/record/DadosEntradaEnfermeiros.java
+++ enfermeiro/src/main/java/com/exampleenfermeiro/record/DadosEntradaEnfermeiros.java
@@ -2,6 +2,7 @@ package com.exampleenfermeiro.record;
 
 import com.exampleenfermeiro.enumm.Laboratorio;
 import com.exampleenfermeiro.enumm.Via;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 
@@ -10,11 +11,13 @@ import java.time.LocalDate;
 public record DadosEntradaEnfermeiros(
         @NotBlank
         String nome,
+        @Enumerated
         Via via,
         @NotBlank
         String lote,
         Integer quantidade,
         @Future
         LocalDate validade,
+        @Enumerated
         Laboratorio laboratorio) {
 }
