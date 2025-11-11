265428c3569224e857f8c28ce732542ae5e34f2b
oliveirabilu <carloscesaroliveira13@hotmail.com>
2025-11-11 18:57:28 -0300

    feature: lOMBOK não funcionou feito tudo pelo generate Getter e Setter, e construtor vazio

--- enfermeiro/src/main/java/com/exampleenfermeiro/entities/Enfermeiro.java
+++ enfermeiro/src/main/java/com/exampleenfermeiro/entities/Enfermeiro.java
@@ -10,8 +10,6 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 @Entity
 @Table(name = "TB_ENFERMEIRO")
-@Data
-@NoArgsConstructor
 public class Enfermeiro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,8 +23,67 @@ public class Enfermeiro {
     @Enumerated(EnumType.STRING)
     private Laboratorio laboratorio;
 
+    public Enfermeiro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Via getVia() {
+        return via;
+    }
+
+    public void setVia(Via via) {
+        this.via = via;
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public void setLote(String lote) {
+        this.lote = lote;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public LocalDate getValidade() {
+        return validade;
+    }
+
+    public void setValidade(LocalDate validade) {
+        this.validade = validade;
+    }
+
+    public Laboratorio getLaboratorio() {
+        return laboratorio;
+    }
+
+    public void setLaboratorio(Laboratorio laboratorio) {
+        this.laboratorio = laboratorio;
+    }
+
     public Enfermeiro(DadosEntradaEnfermeiros dados) {
-        this.nome= dados.nome();
+        this.nome=dados.nome();
         this.via=dados.via();
         this.lote= dados.lote();
         this.quantidade= dados.quantidade();
