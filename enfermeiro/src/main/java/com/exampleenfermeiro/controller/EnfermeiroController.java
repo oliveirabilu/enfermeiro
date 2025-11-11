348ea898a11e46b06202f2eb5a9fc566d00a9a15
oliveirabilu <carloscesaroliveira13@hotmail.com>
2025-11-11 18:20:10 -0300

    feature: Criar e testar aplicação com postman com verbo Post

--- enfermeiro/src/main/java/com/exampleenfermeiro/controller/EnfermeiroController.java
+++ enfermeiro/src/main/java/com/exampleenfermeiro/controller/EnfermeiroController.java
@@ -1,7 +1,10 @@
 package com.exampleenfermeiro.controller;
 
+import com.exampleenfermeiro.entities.Enfermeiro;
 import com.exampleenfermeiro.record.DadosEntradaEnfermeiros;
+import com.exampleenfermeiro.repositories.EnfermeiroRepository;
 import jakarta.validation.Valid;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/enfermeiros")
 public class EnfermeiroController {
-    @PostMapping
-    public  void cadastrar (@RequestBody @Valid DadosEntradaEnfermeiros dados){
-        System.out.println(dados);
+    EnfermeiroRepository enfermeiroRepository;
 
+    public EnfermeiroController(EnfermeiroRepository enfermeiroRepository) {
+        this.enfermeiroRepository = enfermeiroRepository;
+    }
+
+    @PostMapping
+    @Transactional
+    public  void cadastrar (@RequestBody @Valid DadosEntradaEnfermeiros dados){
+        enfermeiroRepository.save(new Enfermeiro(dados));
     }
 }
