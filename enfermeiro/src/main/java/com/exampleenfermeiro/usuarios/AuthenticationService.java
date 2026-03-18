c2cb5e572ce078d3b2398130c755709afd242a3a
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-03-18 10:08:18 -0300

    feature: Criado classe service Authentication e implementado o UserDetailsService, e como implementou tem que levar os metodos junto, vamos criar um retorno

---
+++ enfermeiro/src/main/java/com/exampleenfermeiro/usuarios/AuthenticationService.java
@@ -0,0 +1,19 @@
+package com.exampleenfermeiro.usuarios;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+    public AuthenticationService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(username);
+    }
+}
